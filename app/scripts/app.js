3715d9627592956c499fd3a174f2f9fe0a32cf89
wrayal <wrayal@gmail.com>
2017-04-26 22:02:04 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name step1CreateRepoApp
+ * @description
+ * # step1CreateRepoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('step1CreateRepoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name step1CreateRepoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the step1CreateRepoApp
+ */
+angular.module('step1CreateRepoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
